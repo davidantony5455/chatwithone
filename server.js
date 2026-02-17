1a5aded3473afe175f55d963bc626e3a1a5a6098
davidantony5455 <davidantony5455@gmail.com>
2026-02-17 07:21:23 +0530

    Update server.js
    updated

--- server.js
+++ server.js
@@ -101,3 +101,18 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
